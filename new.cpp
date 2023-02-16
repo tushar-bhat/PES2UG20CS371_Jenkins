cd1b3f0216b3dc5ce7e1f471be96c7b0a2cdf0b3
Tushar Bhat <tusharbhat001@gmail.com>
2023-02-16 21:40:17 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "This is being executed with the help of Jenkins pipeline"
+  cout << "I Tushar Bhat, PES2UG20CS371 have successfully built and run the modified hello.cpp file \n";
+  return 0;
+}
