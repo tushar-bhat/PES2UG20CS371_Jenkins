e16b354ad6bb6cb046dcf65a127cb83cd372109f
Tushar Bhat <tusharbhat001@gmail.com>
2023-02-16 21:14:26 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I Tushar Bhat, PES2UG20CS371 have successfully built and run\n";
+  cout << "Hello, ngrock\n";
+  cout << "I Tushar Bhat, PES2UG20CS371 have successfully built and run the modified hello.cpp file \n";
   return 0;
 }
