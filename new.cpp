9b4ca139d8d6b2d23047a7dc41801f7079e9d8ce
Tushar Bhat <tusharbhat001@gmail.com>
2023-02-16 21:41:02 +0530

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "This is being executed with the help of Jenkins pipeline"
+  cout << "This is being executed with the help of Jenkins pipeline";
   cout << "I Tushar Bhat, PES2UG20CS371 have successfully built and run the modified hello.cpp file \n";
   return 0;
 }
